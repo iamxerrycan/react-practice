f0981dcaaad8f7acea198ca071dd9fe9ab2bcf8c
rajshish333 <rajshish333@gmail.com>
2024-02-23 19:21:44 +0530

    style change

---
+++ twentyfive/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Accordian from './component/accordian/Accordian';
+import Randomcolour from './component/randomcolour/Randomcolour';
+import StartRating from './component/starrating/StartRating';
+
+function App() {
+  return (
+    <div className="App">
+    <Accordian/>
+    <Randomcolour/>
+    <StartRating/>
+    </div>
+  );
+}
+
+export default App;
